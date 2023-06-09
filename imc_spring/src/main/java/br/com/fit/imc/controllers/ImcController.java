6563c10b7609e01569fa342ae8c26b3cfb7aab46
Felipe Santana <fesantana.dev@gmail.com>
2023-06-08 23:21:53 -0300

    atividade spring imc

---
+++ imc_spring/src/main/java/br/com/fit/imc/controllers/ImcController.java
@@ -0,0 +1,24 @@
+package br.com.fit.imc.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fit.imc.models.Person;
+import br.com.fit.imc.services.ImcCalculatorService;
+
+@RestController
+@RequestMapping("/imc")
+public class ImcController {
+    @Autowired
+    private ImcCalculatorService imcService;
+
+    @PostMapping("/calc")
+    public Person calculateImc(@RequestBody Person person) {
+        imcService.calculate(person);
+
+        return person;
+    }
+}
